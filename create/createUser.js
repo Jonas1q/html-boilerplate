b0ead1e2ce13da8ae1109e728872f91fd45d36a8
Jonas1q <42465386+Jonas1q@users.noreply.github.com>
2022-11-30 21:04:39 +0100

    made posting doable

--- create/createUser.js
+++ create/createUser.js
@@ -3,17 +3,17 @@ function enableDarkmode() {
   element.classList.toggle("dark-mode");
 }
 
-$(document).ready(function () {
-  $("#CreateBtn").click(function (event) {
-    var mail = document.getElementById("email").value;
-    var pass = document.getElementById("password").value;
-    var regx = /\S+@\S+\.\S+/;
-    if (regx.test(mail) && pass.length >= 6) {
-      event.preventDefault();
-      window.location = "/login/login.html";
-    } else {
-      event.preventDefault();
-      window.location = "/create/createUser.html";
-    }
-  });
-});
+//$(document).ready(function () {
+//$("#CreateBtn").click(function (event) {
+//var mail = document.getElementById("email").value;
+//var pass = document.getElementById("password").value;
+//var regx = /\S+@\S+\.\S+/;
+//if (regx.test(mail) && pass.length >= 6) {
+//event.preventDefault();
+//alert("clicked");
+//} else {
+//event.preventDefault();
+//window.location = "/create/createUser.html";
+//}
+//});
+//});
